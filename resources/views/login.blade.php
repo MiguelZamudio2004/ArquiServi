8a32eea47bc5f006d319536efea1475dbe8a7564
Aracely Rodriguez <aracelyrodriguez9417@gmail.com>
2026-09-04 14:04:51 -0600

    Style:Mejora diseño y animaciones del login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,34 +18,40 @@
     </div>
 @endif
 
-<header>
-    <h1 id="titulo">ArquiServi</h1>
-    <img src="{{ asset('encabezado.png') }}" class="logo" alt="ArquiServi">
+<header class="encabezado">
+    <img src="{{ asset('encabezado2.png') }}" class="logo" alt="ArquiServi">
 </header>
-
+    <h1 id="titulo">
+        <span style="--i:1">A</span>
+        <span style="--i:2">R</span>
+        <span style="--i:3">Q</span>
+        <span style="--i:4">U</span>
+        <span style="--i:5">I</span>
+        <span style="--i:6">S</span>
+        <span style="--i:7">E</span>
+        <span style="--i:8">R</span>
+        <span style="--i:9">V</span>
+        <span style="--i:10">I</span>
+    </h1>
     <section class="form-login">
         <form action="{{ route('login.auth') }}" method="POST">
             @csrf
-        <h2>Inicio de Sesion</h2>
+        <h2 id="subtitulo">Inicio de sesión</h2>
         <form>
-<<<<<<< HEAD
-                <label for="username">Nombre de Usuario:</label>
-                <input type="text" id="username" placeholder="Ingrese su nombre de usuario" name="correo" value="{{ old('correo') }}" required>
-                <label>Contraseña:</label>
-                <input type="password" placeholder="Ingrese su contraseña" name="password" required>
-            <p>No tiene una cuenta? <a href="/register">Crea una</a></p>
-=======
-                <label class="etiqueta" for="username">Nombre de Usuario:</label>
-                <input type="text" id="username" placeholder="Ingrese su nombre de usuario" required>
-                <label>Contraseña:</label>
-                <input type="password"  place placeholder="Ingrese su contraseña" required>
-            <p>No tienes una cuenta? <a href="/register">Crea una</a></p>
->>>>>>> 971145c (feat: Creacion de CSS)
-            <p>Olvidaste tu contraseña? <a href="/recuperation">Recuperala</a></p>
-            <button type="submit">Iniciar Sesion</button>
+                <label class="etiqueta" for="username">Nombre de usuario:</label>
+                <input class="form-control" type="text" id="username" placeholder="Ingrese su nombre de usuario" required>
+                <label class="etiqueta">Contraseña:</label>
+                <input class="form-control" type="password"  placeholder="Ingrese su contraseña" required>
+            <p class="textalter">¿No tienes una cuenta? <a href="/register">Crea una</a></p>
+            <p class="textalter">¿Olvidaste tu contraseña? <a href="/recuperation">Recuperala</a></p>
+            <button class="btn" type="submit">Iniciar Sesion</button>
             
         </form>
     </section>
 
 </body>
+
+<footer class="pie">
+    <p>© 2024 ArquiServi. Todos los derechos reservados.</p>
+</footer>
 </html>
