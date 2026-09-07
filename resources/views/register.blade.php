4aa35120b01cc2afa345d2ff5324dc9d835e9a6e
Aracely Rodriguez <aracelyrodriguez9417@gmail.com>
2026-09-07 12:18:17 -0600

    Style: Visualización del registro

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,15 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="icon" href="{{ asset('icono.png') }}" type="image/png">
 </head>
 
 <body>
+    <header class="encabezado">
+    <img src="{{ asset('encabezado2.png') }}" class="logo" alt="ArquiServi">
+    </header>
 
-    <section>
+    <section class="form-register">
 
         <form action="{{ route('register.store') }}" method="POST">
             @csrf
-        <h2>Nueva Cuenta</h2>
+        <h2 id="subtitulo">Nueva Cuenta</h2>
             @if ($errors->any())
                 <div>
                     <ul>
@@ -24,42 +29,42 @@
                 </div>
             @endif
 
-            <label>Nombre:</label>
-            <input type="text" name="nombre" placeholder="Ingrese su nombre" value="{{ old('nombre') }}" required>
+            <label class="etiqueta">Nombre:</label>
+            <input class="form-control" type="text" name="nombre" placeholder="Ingrese su nombre" value="{{ old('nombre') }}" required>
 
-            <label>Apellido paterno:</label>
-            <input type="text" name="apellido_paterno" placeholder="Ingrese su apellido paterno" value="{{ old('apellido_paterno') }}" required>
+            <label class="etiqueta">Apellido paterno:</label>
+            <input class="form-control" type="text" name="apellido_paterno" placeholder="Ingrese su apellido paterno" value="{{ old('apellido_paterno') }}" required>
 
-            <label>Apellido materno:</label>
-            <input type="text" name="apellido_materno" placeholder="Ingrese su apellido materno" value="{{ old('apellido_materno') }}" required>
+            <label class="etiqueta">Apellido materno:</label>
+            <input class="form-control" type="text" name="apellido_materno" placeholder="Ingrese su apellido materno" value="{{ old('apellido_materno') }}" required>
 
-            <label>Correo electrónico:</label>
-            <input type="email" name="correo" placeholder="Ingrese su correo electrónico" value="{{ old('correo') }}" required>
+            <label class="etiqueta">Correo electrónico:</label>
+            <input class="form-control" type="email" name="correo" placeholder="Ingrese su correo electrónico" value="{{ old('correo') }}" required>
 
-            <label>Número de teléfono:</label>
-            <input type="tel" name="telefono" placeholder="Ingrese su número de teléfono" value="{{ old('telefono') }}" required>
+            <label class="etiqueta">Número de teléfono:</label>
+            <input class="form-control" type="tel" name="telefono" placeholder="Ingrese su número de teléfono" value="{{ old('telefono') }}" required>
 
-            <label>Rol:</label>
-            <select name="rol" required>
-            <option value="" disabled {{ old('rol') ? '' : 'selected' }}>
+            <label class="etiqueta">Rol:</label>
+            <select  class="form-control" name="rol" required>
+            <option class="text-alter" value="" disabled {{ old('rol') ? '' : 'selected' }}>
                 Seleccione un rol
             </option>
 
-                <option
+                <option class="form-control"
                     value="usuario"
                     {{ old('rol') == 'usuario' ? 'selected' : '' }}
                 >
                     Cliente
                 </option>
 
-                <option
+                <option class="form-control"
                     value="profesional"
                     {{ old('rol') == 'profesional' ? 'selected' : '' }}
                 >
                     Profesional
                 </option>
 
-                <option
+                <option class="form-control"
                     value="proveedor"
                     {{ old('rol') == 'proveedor' ? 'selected' : '' }}
                 >
@@ -67,20 +72,24 @@
                 </option>
             </select>
 
-            <label>Contraseña:</label>
-            <input type="password" name="password" placeholder="Ingrese su contraseña" required>
+            <label class="etiqueta">Contraseña:</label>
+            <input class="form-control" type="password" name="password" placeholder="Ingrese su contraseña" required>
 
-            <label>Confirmar contraseña:</label>
-            <input type="password" name="password_confirmation" placeholder="Confirme su contraseña" required>
+            <label class="etiqueta">Confirmar contraseña:</label>
+            <input class="form-control" type="password" name="password_confirmation" placeholder="Confirme su contraseña" required>
 
-            <p>¿Ya tienes una cuenta? <a href="/login">Inicia sesión</a></p>
+            <p class="text-alter">¿Ya tienes una cuenta? <a href="/login">Inicia sesión</a></p>
 
-            <button type="submit">Siguiente</button>
+            <button class="btn" type="submit">Siguiente</button>
 
         </form>
 
     </section>
 
 </body>
+
+<footer class="pie">
+    <p>© 2024 ArquiServi. Todos los derechos reservados.</p>
+</footer>
 
 </html>
