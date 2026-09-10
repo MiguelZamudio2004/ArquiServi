275f2e029bf7b3e965ac9c3c21dce686d6b421dd
Aracely Rodriguez <aracelyrodriguez9417@gmail.com>
2026-09-10 12:35:44 -0600

    Style: Menu principal con diseño

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,50 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+    <link rel="icon" href="{{ asset('icono.png') }}" type="image/png">
 </head>
 
 <body>
-    <h2> Bienvenido a ArquiServi {{ Auth::user()->nombre }}</h2>
-    <label> ¿Qué servicio necesitas hoy? </label>
-    <select>
-        <option disabled selected> Selecciona tu servicio requerido </option>
-    </select>
-    <button>Ver catálogo completo de servicios</button>
-    <label>Inicia sesión para ver detalles de tu cuenta</label>
-    <button><a href="{{ route('login') }}">Iniciar sesión</a></button>
+    <header class="encabezado">
+    <img src="{{ asset('encabezado2.png') }}" class="logo" alt="ArquiServi">
+    </header>
 
+    <h2 class="bienvenida"> Bienvenido(a)</h2>
+    <h2 class="bienvenida">
+        <span style="--i:1">A</span>
+        <span style="--i:2">R</span>
+        <span style="--i:3">Q</span>
+        <span style="--i:4">U</span>
+        <span style="--i:5">I</span>
+        <span style="--i:6">S</span>
+        <span style="--i:7">E</span>
+        <span style="--i:8">R</span>
+        <span style="--i:9">V</span>
+        <span style="--i:10">I</span>
+    </h2>
+    <h2 class="bienvenida"> {{ Auth::user()->nombre }} </h2>
 
-    <table>
-        <tr>
-            <th>Nombre</th>
-            <th>Especialidad</th>
-            <th>Calificación</th>
-            <th> </th>
-        </tr>
-        <tr>
-            <td> Esteban Hernandez</td>
-            <td>Muebles</td>
-            <td>4.5</td>
-            <td><button>Ver perfil</button></td>
-        </tr>
-        <tr>
-            <td>Raul Jimenez</td>
-            <td>Cocinas integrales</td>
-            <td>3.2</td>
-            <td><button>Ver perfil</button></td>
-        </tr>
-        <tr>
-            <td>Jorge Reyes</td>
-            <td>Closets</td>
-            <td>4.8</td>
-            <td><button>Ver perfil</button></td>
-        </tr>
-        <tr>
-            <td>Maria Contreras</td>
-            <td>Puertas de madera</td>
-            <td>4.0</td>
-            <td><button>Ver perfil</button></td>
-        </tr>
-    </table>
+    <section class="contenedor-principal">
+        <section class="contenedor-form">
+            <label class="etiqueta">¿Qué servicio necesitas hoy?</label>
+            <select class="form-control">
+            <option disabled selected>Selecciona tu servicio requerido</option>
+            </select>
+            <button class="btn">Ver catálogo completo de servicios</button>
+            <label class="etiqueta">Inicia sesión para ver detalles de tu cuenta</label>
+            <button class="btn"><a href="{{ route('login') }}">Iniciar sesión</a></button>
+        </section>
+
+        <section class="contenedor-imagen">
+            <img src="{{ asset('menu.png') }}" alt="Imagen de bienvenida" class="imagen-bienvenida">
+        </section>
+    </section>
+
 </body>
+
+<footer class="pie">
+    <p>© 2026 ArquiServi. Todos los derechos reservados.</p>
+</footer>
+
 </html>
